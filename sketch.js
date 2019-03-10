f809af1d08496f09674dbcb146792f31efc445dc
Alex Diwa <atdiwa@gmail.com>
2019-03-10 17:49:54 +1100

    changed colour scheme

--- sketch.js
+++ sketch.js
@@ -3,27 +3,25 @@ var col1;
 var col2;
 var col3;
 var colours;
+var lineArc;
 
 function setup() {
-	createCanvas(600, 600, SVG);
-
+	createCanvas(600, 600);
 	angleMode(DEGREES);
 	colorMode(HSL)
-	background(15, 100, 56);
-	col1 = color(359, 0, 88);
-	col2 = color(243, 99, 27)
-	col3 = color(16, 72, 51);
+	background(17, 33, 95);
+	col1 = color(17, 4, 61);
+	col2 = color(244, 87, 35);
+	col3 = color(17, 100, 49);
 	colours = [col1, col2, col3];
 }
 
 function draw() {
-		
-
 	for (let j = 0; j < 5; j++) {
 		rings[j] = new Arc;
 		rings[j].show();
 	}
-noLoop();
+	noLoop();
 }
 
 class Arc {
@@ -38,15 +36,21 @@ class Arc {
 		noFill();
 		stroke(colours[floor(random(0, 3))]);
 		strokeWeight(0.5);
-for (let i = 0+this.off; i < this.off+(360/this.ring); i+= 360/this.c) {
-	push();
-	this.r = noise(i)*15 + this.rscale;
-	this.x = this.r * cos(i) + (width/2);
-	this.y = this.r * sin(i) + (width/2);
-	translate(this.x, this.y);
-	rotate(i+random(80, 100));
-	rect(0, 0, 3, 40);
-	pop();
+		for (let i = 0 + this.off; i < this.off + (360/this.ring); i+= 360/this.c) {
+			push();
+			this.r = noise(i) * 15 + this.rscale;
+			this.x = this.r * cos(i) + (width/2);
+			this.y = this.r * sin(i) + (width/2);
+			translate(this.x, this.y);
+			rotate(i + random(80, 100));
+			rect(0, 0, 3, 40);
+			pop();
+		}
 	}
 }
+
+function keyTyped() {
+	if (key === 's') {
+			save('canvas.png');
+  }
 }
